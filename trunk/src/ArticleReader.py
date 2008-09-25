44f8ad5efd872773ec7e748ad47fe8c875948b01
rafael.gcamara <rafael.gcamara@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-09-25 23:10:55 +0000

    Alterações feitas para que seja possivel comparar em 2 listas, a de descritores e de sinonimos

--- trunk/src/ArticleReader.py
+++ trunk/src/ArticleReader.py
@@ -40,42 +40,83 @@ class ArticleReader():
     def prepar_conteudo(self):
         self.filter_and_steam()    
         self.conteudo = self.conteudo.split()
-    def compare(self,lista):
+    def compare(self,listaDesc , listaSin):
         for i in range(len(self.conteudo)):
+
             #Uma Palavra
             print self.conteudo[i]+'\n'
-            if lista.has_key(self.conteudo[i]):
-                if(self.descritores.__contains__(self.conteudo[i])):
-                    self.descritores[self.conteudo[i]] += 1
+            #Descritores
+            if listaDesc.has_key(self.conteudo[i]):
+                if(self.descritores.__contains__(listaDesc[self.conteudo[i]])):
+                    self.descritores[listaDesc[self.conteudo[i]]] += 1
                 else:
-                    self.descritores[self.conteudo[i]] = 1
+                    self.descritores[listaDesc[self.conteudo[i]]] = 1
+                            
+             #sinoniomos       
+            if listaSin.has_key(self.conteudo[i]):
+                if(self.descritores.__contains__(listaDesc[listaSin[self.conteudo[i]]])):
+                    self.descritores[listaDesc[listaSin[self.conteudo[i]]]] += 1
+                else:
+                    self.descritores[listaDesc[listaSin[self.conteudo[i]]]] = 1
+ 
+ 
+ 
+ 
+ 
+            #listaDesc[self.conteudo[i]+' '+self.conteudo[i+1]]
             #Duas Palavras
             if i+1 < len(self.conteudo):
                 print self.conteudo[i]+' '+self.conteudo[i+1]+'\n'
-                if lista.has_key(self.conteudo[i]+' '+self.conteudo[i+1]):
-                    if(self.descritores.__contains__(self.conteudo[i]+' '+self.conteudo[i+1])):
-                        self.descritores[self.conteudo[i]+' '+self.conteudo[i+1]] += 1
+                #Descritores
+                if listaDesc.has_key(self.conteudo[i]+' '+self.conteudo[i+1]):
+                    if(self.descritores.__contains__(listaDesc[self.conteudo[i]+' '+self.conteudo[i+1]])):
+                        self.descritores[listaDesc[self.conteudo[i]+' '+self.conteudo[i+1]]] += 1
                     else:
-                        self.descritores[self.conteudo[i]+' '+self.conteudo[i+1]] = 1    
+                        self.descritores[listaDesc[self.conteudo[i]+' '+self.conteudo[i+1]]] = 1          
+   
+                #sinonimos
+                if listaSin.has_key(self.conteudo[i]+' '+self.conteudo[i+1]):
+                    if(self.descritores.__contains__(listaDesc[listaSin[self.conteudo[i]+' '+self.conteudo[i+1]]])):
+                        self.descritores[listaDesc[listaSin[self.conteudo[i]+' '+self.conteudo[i+1]]]] += 1
+                    else:
+                        self.descritores[listaDesc[listaSin[self.conteudo[i]+' '+self.conteudo[i+1]]]] = 1  
+   
+   
+   
+   
+                        
+            #listaDesc[self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]]
             #Tres Palavras      
             if i+2 < len(self.conteudo):
                 print self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]+'\n'
-                if lista.has_key(self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]):
-                    if(self.descritores.__contains__(self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2])):
-                        self.descritores[self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]] += 1
+                #Descritores
+                if listaDesc.has_key(self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]):
+                    if(self.descritores.__contains__(listaDesc[self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]])):
+                        self.descritores[listaDesc[self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]]] += 1
                     else:
-                        self.descritores[self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]] = 1
+                        self.descritores[listaDesc[self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]]] = 1
+ 
+ 
+ 
+                #sinonimos
+                if listaSin.has_key(self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]):
+                    if(self.descritores.__contains__(listaDesc[listaSin[self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]]])):
+                        self.descritores[listaDesc[listaSin[self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]]]] += 1
+                    else:
+                        self.descritores[listaDesc[listaSin[self.conteudo[i]+' '+self.conteudo[i+1]+' '+self.conteudo[i+2]]]] = 1
+ 
+ 
         
            
 
-descritores = {'HIV': 'HIV' ,
-               'nitric' : 'nitric',
-               'nitric oxid' : 'nitric oxid',
-               'cell' : 'cell' ,
-               'acid' : 'acid'         
+descritores = {'HIV': 'HIV Full String' ,
+               'nitric' : 'Nitric full',
+               'nitric oxid' : 'Nitric oxide full',
+               'cell' : 'cell Full String' ,
+               'acid' : 'acid Full String'         
             }
-sinonimos = {'Acquired Immune Deficiency Syndrome Virus' : 'HIV' ,
-             'Acquired Immunodeficiency Syndrome Virus' : 'HIV' ,
+sinonimos = {'Acquired Immune Deficiency Syndrome Virus' : 'nitric oxid' ,
+             'ablat' : 'nitric oxid' ,
              'AIDS Virus' : 'HIV' ,
              'HTLV-III' :  'HIV' , 
              'Human Immunodeficiency Virus' :  'HIV' }
@@ -95,12 +136,11 @@ ar.set_word_filter(wf)
 
 
 # Arquivo de Entrada
-ar.set_file(r'..\..\in\Entrada.txt')
+ar.set_file(r'..\in\Entrada.txt')
 ar.open_file()
 
 ar.prepar_conteudo()
-ar.compare(descritores)
-ar.compare(sinonimos)
+ar.compare(descritores,sinonimos)
 
 print descritores
 print ar.descritores
